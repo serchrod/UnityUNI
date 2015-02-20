7db20d0ceaf8ca32c4d718af8071ff6a4e333f10
Ronald Flores <burzumumbra@gmail.com>
2015-02-20 12:01:41 -0600

    Update 1.1.3.1
    Main Scene

--- Assets/Scripts/DestroyEnemy.cs
+++ Assets/Scripts/DestroyEnemy.cs
@@ -13,19 +13,9 @@ public class DestroyEnemy : MonoBehaviour {
 	
 	}
 
-	void OnTriggerEnter2D(Collider2D otro){
-		if (otro.tag == "Enemy") {
-			//NotificationCenter.DefaultCenter().PostNotification(this,"personajeMurio");
-			GameObject enemigo = GameObject.Find("EnemigoAzul");
-			enemigo.SetActive(false);
-		}else if(otro.tag == "Enemy"){
-			GameObject enemigo = GameObject.Find("EnemigoNaranja");
-			enemigo.SetActive(false);
-		}else if(otro.tag == "Enemy"){
-			GameObject enemigo = GameObject.Find("EnemigoRojo");
-			enemigo.SetActive(false);
-		}else {
-			//Destroy (otro.gameObject);
+	private void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "Enemy") {
+			Destroy(collision.gameObject);
 		}
 	}
 }
