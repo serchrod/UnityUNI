0ce5a2a7ec00e02d305535e910e67adab33c83b1
Ronald Flores <burzumumbra@gmail.com>
2015-02-22 10:09:00 -0600

    Update 1.2.1.3
    Comentado Script Distancia.

--- Assets/Scripts/distancia.cs
+++ Assets/Scripts/distancia.cs
@@ -1,34 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+	Para obtener la distancia se crea un header, o un apuntador a la diferencia entre la propidad transfom.position de
+	dos objetos, esa diferencia es la magnitud del header lo que nos proporciona la distancia.
+*/
+
 public class distancia : MonoBehaviour {
-	protected Transform playerTransform;
-	protected Transform anclaTransform;
-	GameObject Jugador;
-	GameObject Ancla;
-	protected Vector2 heading;
-	protected float distanciaMagnitud;
-	public int dista;
-	public int ajustarDistancia;
-	public TextMesh dis;
+	protected Transform playerTransform;// Variable quealmacena la propiedad transmform del personaje.
+	protected Transform anclaTransform;// Variable quealmacena la propiedad transmform del objeto ancla.
+	GameObject Jugador;// Variable del GameObject del personaje.
+	GameObject Ancla;// Variable del GameObject del ancla.
+	protected Vector2 heading;// Variable del apuntador que contiene la magnitud de la distancia.
+	protected float distanciaMagnitud;// Variable que almacena el valor en real de la magnitud.
+	public int dista;// Variable que almacena el valor redondeado de la magnitud.
+	public int ajustarDistancia;// Variable que corrije el valor inicial de la distancia.
+	public TextMesh dis;// Variable del objeto texto que recibe la distancia.
 	void Start(){
 	
 	}
 
 	void Update() {
-		medida ();
-		dis.text = dista.ToString () + "m";
+		medida ();// Llamada a a funcion de medicion.
+		dis.text = dista.ToString () + "m";// Retorno de la distanca al objeto de texto, formateando a String.
 	}
 
 	void medida(){
-		Jugador = GameObject.FindGameObjectWithTag("Player");
-		Ancla = GameObject.FindGameObjectWithTag("Ancla");
+		Jugador = GameObject.FindGameObjectWithTag("Player");//Obteniendo el GameObject del personaje.
+		Ancla = GameObject.FindGameObjectWithTag("Ancla");//Obteniendo el GameObject del ancla.
 
-		playerTransform = Jugador.transform;
-		anclaTransform = Ancla.transform;
-		heading = anclaTransform.position - playerTransform.position;
-		distanciaMagnitud = heading.magnitude;
-		dista = (Mathf.RoundToInt(distanciaMagnitud)- ajustarDistancia);
+		playerTransform = Jugador.transform;//Obteniendo la propiedad transform del GameObject del personaje.
+		anclaTransform = Ancla.transform;//Obteniendo la propiedad transform del GameObject del ancla.
+		heading = anclaTransform.position - playerTransform.position;// Calculando la magnitud de la distancia.
+		distanciaMagnitud = heading.magnitude;// Asignando la magnitud a la variable que almacena la distancia.
+		dista = (Mathf.RoundToInt(distanciaMagnitud)- ajustarDistancia);// Redondeando y ajustando la distancia.
 
 	}
 }
