c465732e83f68e64fccda5f374f6deb3125d918e
Ronald Flores <burzumumbra@gmail.com>
2015-02-22 08:08:19 -0600

    Update.1.2
    Distancia en Metros

--- Assets/Scripts/distancia.cs
+++ Assets/Scripts/distancia.cs
@@ -2,24 +2,32 @@
 using System.Collections;
 
 public class distancia : MonoBehaviour {
-	public float distfinal;
 	protected Transform playerTransform;
-	protected Vector2 destino;
+	protected Transform anclaTransform;
+	GameObject Jugador;
+	GameObject Ancla;
+	protected Vector2 heading;
+	protected float distanciaMagnitud;
+	public int dista;
+	public TextMesh dis;
+	void Start(){
+	
+	}
 
-
-	void Update () {
+	void Update() {
 		medida ();
+		dis.text = dista.ToString () + "m";
 	}
 
 	void medida(){
-		GameObject objPlayer = GameObject.FindGameObjectWithTag("Wandar");
-		playerTransform = objPlayer.transform;
-		destino = playerTransform.position;
+		Jugador = GameObject.FindGameObjectWithTag("Player");
+		Ancla = GameObject.FindGameObjectWithTag("Ancla");
 
-
-		if (Vector2.Distance(transform.position, destino) != 0.0f) {
-			print(transform.position.x);
-		}
+		playerTransform = Jugador.transform;
+		anclaTransform = Ancla.transform;
+		heading = anclaTransform.position - playerTransform.position;
+		distanciaMagnitud = heading.magnitude;
+		dista = (Mathf.RoundToInt(distanciaMagnitud)-2);
 
 	}
 }
