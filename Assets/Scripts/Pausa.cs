6d3f43cb74c3db7c3b579cb3c4a588731e6d6ef2
Ronald Flores <burzumumbra@gmail.com>
2015-03-02 12:26:40 -0600

    Update 1.4
    Pausa

---
+++ Assets/Scripts/Pausa.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pausa : MonoBehaviour {
+	bool pausa = false;
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((!pausa) && Input.GetKeyDown ( KeyCode.P)) {
+			Time.timeScale = 0;
+			audio.Pause();// Detiene el audio de la camara principal.
+			pausa = true;
+		}else if ((pausa) && Input.GetKeyDown ( KeyCode.P)){
+			Time.timeScale = 1;
+			audio.Play();// Reproduce el audio del objeto que usa el script.
+			pausa = false;
+		}
+	}
+}
